6710b7e4bc49159b4fb2f777d5ba37ccbefbb0eb
Dan Schaefer <dan@schaeferzone.net>
2016-02-06 19:16:29 -0500

    add AttendeesController and route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,11 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'api/v1'], function()
+{
+    Route::resource('attendee', 'AttendeesController');
 });
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
