974efd6edde460c6d3eb35cbb23983a589153fb0
Dan Schaefer <dan@schaeferzone.net>
2016-02-20 15:27:02 -0500

    add api tests
    Change routes to use plural nouns.
    Update phpunit.
    Add a database config for testing.
    Add a framework and helpers for testing the API.
    Add tests for Attendees and Counselors.
    Remove the Example test.

---
+++ tests/AttendeesTest.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AttendeesTest extends ApiTester
+{
+    use Factory;
+
+    public function getStub()
+    {
+        return [
+            'first_name' => $this->faker->firstNameMale(),
+            'last_name'  => $this->faker->lastName(),
+            'suffix'     => $this->faker->suffix(),
+            'identifier' => $this->faker->regexify('[A-Z]?[0-9]{3}/[0-9]{3}'),
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_fetches_all_attendees()
+    {
+        //arrange
+        $this->times(5)->make('App\Attendee');
+
+        //act
+        $this->getJson('/api/v1/attendees/');
+
+        //assert
+        $this->assertResponseOk();
+    }
+
+    /**
+     * @test
+     */
+    public function it_fetches_a_single_attendee()
+    {
+        //arrange
+        $this->make('App\Attendee');
+
+        //act
+        $attendee = $this->getJson('/api/v1/attendees/1')->data;
+
+        //assert
+        $this->assertResponseOk();
+        $this->assertObjectHasAttributes($attendee, 'first_name', 'last_name', 'suffix', 'identifier');
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_a_404_if_an_attendee_is_not_found()
+    {
+        //act
+        $this->getJson('/api/v1/attendees/x');
+
+        //assert
+        $this->assertResponseStatus(404);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_a_new_attendee_given_valid_parameters()
+    {
+        //act
+        $this->getJson('/api/v1/attendees', 'POST', $this->getStub());
+
+        //assert
+        $this->assertResponseStatus(201);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_a_422_if_a_new_attendee_request_fails_validation()
+    {
+        //act
+        $this->getJson('/api/v1/attendees', 'POST');
+
+        //assert
+        $this->assertResponseStatus(422);
+    }
+}
